93fc09cb94ca5ea87e01e1e942f9c0f192c34361
avernikoz <avernikoz@gmail.com>
2024-04-22 22:50:41 +0300

    add export of validation functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./coin/CoinManager";
 export * from "./coin/types";
 export * from "./coin/utils/validation/invalid-param-errors";
+export * from "./coin/utils/validation/validation";
