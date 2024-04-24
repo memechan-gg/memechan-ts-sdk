911f20b128e63a4ca6a8a0c0d7704594e2fd0482
diciaup <byte43@proton.me>
2024-04-24 11:15:34 +0200

    fix: linting issues

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  ignorePatterns: [],
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json"],
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  rules: {
+    quotes: ["error", "double"],
+    "import/no-unresolved": 0,
+    indent: "off",
+    "max-len": ["error", { code: 120 }],
+    "prettier/prettier": 2, // Means error
+  },
+  overrides: [
+    {
+      files: ["tests/**/*"],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
