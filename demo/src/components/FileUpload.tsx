d78aa3316f6d75429b447b0e3b455184eb660fae
diciaup <byte43@proton.me>
2024-04-24 13:09:07 +0200

    chore: updated readme

--- demo/src/components/FileUpload.tsx
+++ demo/src/components/FileUpload.tsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 import { Auth, CoinAPI } from "@avernikoz/memechan-ts-sdk";
 import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
-=======
-import { CoinAPI } from "@avernikoz/memechan-ts-sdk";
->>>>>>> 71d01fdbb10f959b8125fcff6a29f8e33657ecfc
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const api = new CoinAPI();
 
@@ -16,21 +12,25 @@ export const FileUpload: React.FC = () => {
     setFile(selectedFile);
   };
 
+  useEffect(() => {
+    authenticate();
+  }, []);
+
+  const authenticate = async () => {
+    const keypair = new Ed25519Keypair();
+    console.log("Testing with wallet", keypair.getPublicKey().toSuiAddress());
+    const authService = new Auth();
+    const messageToSign = await authService.requestMessageToSign(keypair.getPublicKey().toSuiAddress());
+    const { signature } = await keypair.signPersonalMessage(new TextEncoder().encode(messageToSign));
+    await authService.refreshSession({
+      walletAddress: keypair.getPublicKey().toSuiAddress(),
+      signedMessage: signature,
+    });
+    console.log("Wallet authenticated");
+  };
+
   const handleUpload = async () => {
     if (file) {
-<<<<<<< HEAD
-      const keypair = new Ed25519Keypair();
-      console.log("Testing with wallet", keypair.getPublicKey().toSuiAddress());
-      const authService = new Auth();
-      const messageToSign = await authService.requestMessageToSign(keypair.getPublicKey().toSuiAddress());
-      const { signature } = await keypair.signPersonalMessage(new TextEncoder().encode(messageToSign));
-      await authService.refreshSession({
-        walletAddress: keypair.getPublicKey().toSuiAddress(),
-        signedMessage: signature,
-      });
-      console.log("Wallet authenticated");
-=======
->>>>>>> 71d01fdbb10f959b8125fcff6a29f8e33657ecfc
       console.log("Uploading file:", file.name);
       await api.uploadFile(file);
       console.log("File uploaded");
