2045142e51f750672b5fd8cf0627b936bbb68778
avernikoz <avernikoz@gmail.com>
2024-04-24 04:19:08 +0300

    add swap sui for ticket method to bonding curve

--- src/bonding-pool/types.ts
+++ src/bonding-pool/types.ts
@@ -1,5 +1,7 @@
 import { TransactionBlock } from "@mysten/sui.js/transactions";
 import { CreateCoinTransactionParams } from "../coin/types";
+import { ObjectArg } from "@avernikoz/memechan-ts-interface/dist/_framework/util";
+import BigNumber from "bignumber.js";
 
 export type ExtractedCoinDataFromTransaction = {
   memeCoin: {
@@ -30,3 +32,14 @@ export type CreateCoinTransactionParamsWithoutCertainProps = Omit<
   CreateCoinTransactionParams,
   "decimals" | "fixedSupply" | "mintAmount"
 >;
+
+export type SwapSuiForTicketParams = {
+  memeCoin: { coinType: string };
+  ticketCoin: { coinType: string };
+  transaction?: TransactionBlock;
+
+  // swap params
+  bondingCurvePoolObjectId: ObjectArg;
+  inputSuiAmount: string;
+  minOutputTicketAmount: string;
+};
