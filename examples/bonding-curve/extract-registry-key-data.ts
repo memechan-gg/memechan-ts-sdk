155a0a96ead6e7759ae8b1ceb3c2060fe0f8647d
avernikoz <avernikoz@gmail.com>
2024-04-28 02:09:50 +0300

    upd extractRegistryKeyDataExample example

--- examples/bonding-curve/extract-registry-key-data.ts
+++ examples/bonding-curve/extract-registry-key-data.ts
@@ -4,7 +4,7 @@ import { extractRegistryKeyData } from "../../src/bonding-pool/utils/extractRegi
 export const extractRegistryKeyDataExample = () => {
   const registryKeyTypenameData =
     // eslint-disable-next-line max-len
-    "8f9a0538e30a67e900fe0db14ed6845b72e1f89378f204c2f3ba5b25eadc7fd1::index::RegistryKey<8f9a0538e30a67e900fe0db14ed6845b72e1f89378f204c2f3ba5b25eadc7fd1::curves::Bound,d213f1c1871239fbd154e303f68c6ccd135a8c0673f612d605242d4edc275614::ac_b_test_token_4am::AC_B_TEST_TOKEN_4AM,0000000000000000000000000000000000000000000000000000000000000002::sui::SUI>";
+    "9e1701ec8d7942a79874a40d4d5c9d94c45ffd141c9cd2cff4f4fc3820329b61::index::RegistryKey<a4f0b25689b64b5c169d18bd7f57b24ce65a2c737744709b4e44b01e7247760b::ticket_test_token_4am::TICKET_TEST_TOKEN_4AM,0000000000000000000000000000000000000000000000000000000000000002::sui::SUI,5055576faed90f8f32db5d88cb286036ce6df5b77f1edb8544ec11b0f3da4370::test_token_4am::TEST_TOKEN_4AM>";
 
   const data = extractRegistryKeyData(registryKeyTypenameData);
 
