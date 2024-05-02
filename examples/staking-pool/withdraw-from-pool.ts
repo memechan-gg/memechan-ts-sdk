de5f8751d5dcca0644e0188fbe897d91705094ab
diciaup <byte43@proton.me>
2024-05-02 17:56:53 +0200

    developing staking pool parsing from tx

--- examples/staking-pool/withdraw-from-pool.ts
+++ examples/staking-pool/withdraw-from-pool.ts
@@ -1,13 +1,13 @@
 import { keypair, provider, user } from "../common";
 import { StakingPool } from "../../src";
 import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { getFullnodeUrl, SuiClient } from "@mysten/sui.js/client";
 
 // yarn tsx examples/staking-pool/withdraw-from-pool.ts
 export const withdrawFromPool = async () => {
-  const stakingPool = new StakingPool({
-    memeCoinType: "0x5a63a58787c6ebc1faf9741a6ef9292020b2a02278a8d23b15c03938aadb8237::test_token_4am::TEST_TOKEN_4AM",
-    lpCoinType: "",
-    address: "0xa867022657c563d23e34b5d4557605f8347e4a214053e98268411e297efdd1e9",
+  const stakingPool = await StakingPool.fromGoLiveDefaultTx({
+    txDigest: "9nC4RG4ma6mLf9GciXSn2fHi4SPuKrGsieGyqvAc6EY3",
+    provider: new SuiClient({ url: getFullnodeUrl("mainnet") }),
   });
 
   const tx = new TransactionBlock();
