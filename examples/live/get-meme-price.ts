bdbb777216db3a744b2cd455db2808878dc8045d
bathord <alutskin@bk.ru>
2024-05-07 00:06:17 +0300

    Create examples for live pools

---
+++ examples/live/get-meme-price.ts
@@ -0,0 +1,23 @@
+import { LiveCLMM } from "../../src";
+import { provider } from "../common";
+
+// yarn tsx examples/live/get-meme-price.ts
+export const getMemePrice = async () => {
+  const memeCoinType =
+    "0xf8a2ba07ec67b0f4ad7458a6d58af6fa7b00374e21d6ae45e3dbe0cab0f78865::meme_06_05_2024_03::MEME_06_05_2024_03";
+
+  const liveInstance = new LiveCLMM({
+    provider,
+    data: {
+      memeCoin: {
+        coinType: memeCoinType,
+      },
+    },
+  });
+
+  const { priceInSui, priceInUsd } = await liveInstance.getMemeCoinPrice(memeCoinType);
+  console.debug("priceInSui: ", priceInSui);
+  console.debug("priceInUsd: ", priceInUsd);
+};
+
+getMemePrice();
